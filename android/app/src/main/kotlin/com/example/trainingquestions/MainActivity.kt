73be9421b1a7fe09bf4bf60fb0741c3dee33aa36
Otavio Freitas <otavioj105@gmail.com>
2024-04-01 17:24:13 -0300

    Fist commit

---
+++ android/app/src/main/kotlin/com/example/trainingquestions/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trainingquestions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+main() => runApp(const AppQuestions()); 
+
+class AppQuestions extends StatefulWidget {
+  const AppQuestions({super.key});
+
+  @override
+  State<AppQuestions> createState() => _AppQuestionsState();
+}
+
+class _AppQuestionsState extends State<AppQuestions> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
